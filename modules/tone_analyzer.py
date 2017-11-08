d41b7a9ad6ee2dfa53779c96d0c0756bb3d32bec
Nur Muhammad Shafiullah <nshafiul@mit.edu>
2017-11-07 22:26:31 -0500

    Added tone analyzer using IBM api

--- modules/tone_analyzer.py
+++ modules/tone_analyzer.py
@@ -1,3 +1,15 @@
-from secrets import SECRET_USERNAME
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from secrets import SECRET_USERNAME, SECRET_PASSWORD
 
-print (SECRET_USERNAME)
+def analyze_emotion(text):
+    tone_analyzer = ToneAnalyzerV3(
+                        version='2017-09-21',
+                        username=SECRET_USERNAME,
+                        password=SECRET_PASSWORD,
+                    )
+    tone = tone_analyzer.tone(text, tones='emotion', content_type='text/plain')
+    return tone
+
+if __name__ == '__main__':
+    print(analyze_emotion("Everything changes, but nothing is truly lost."))
